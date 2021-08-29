5971cf576d91a065096a9cbff464f6d6fd4d4895
Dr. Delyan Malchev <39366200+DrMalchev@users.noreply.github.com>
2021-08-29 21:06:31 +0300

    Working Search function added

--- Web_Programming_with_Python_and_JavaScript/Project_1_Wiki/wiki/encyclopedia/views.py
+++ Web_Programming_with_Python_and_JavaScript/Project_1_Wiki/wiki/encyclopedia/views.py
@@ -1,10 +1,13 @@
+from re import template
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
 from . import util
 
 import markdown2 # underlined yellow but works fine for me. no action needed.
-
+#models.py
+from .models import *
+from django.http.response import HttpResponseRedirect
 
 
 def index(request):
@@ -24,5 +27,33 @@ def title(request, title_name):
         "entries": util.list_entries()
     })
     
+def search(request):
+    #if request.method == "GET":
         
+    
+    search_key=request.POST.get('q')
+    return HttpResponseRedirect(f"wiki/{search_key}")
+    #entries = [x.lower() for x in util.list_entries()]
+    #find_entries = list()
+
+    #search_key = request.POST.get('q',"")
+    #return render(request, "encyclopedia/search.html", {"no_result": f"No results for {search_key}"})
+        
+
+    #if search_key.lower() in entries:
+        #return HttpResponseRedirect(f"wiki/{search_key}")
+    #return HttpResponseRedirect("wiki/css")
+        
+    """ for entry in entries:
+        if search_box in entry:
+            find_entries.append(entry)
+        else:
+            print(f'{find_entries}')
+    if find_entries:
+        return render(request, "encyclopedia/search.html", {
+          "search_result": find_entries,
+          "search": search_box
+    })
+    else:
+        return render(request, "encyclopedia/search.html", {"no_result": f"No results for {search_box}"}) """
         
