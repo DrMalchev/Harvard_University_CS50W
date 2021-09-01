8c879cd555d0d60ea012ae5334805e3190e94038
Dr. Delyan Malchev <39366200+DrMalchev@users.noreply.github.com>
2021-09-01 18:45:30 +0300

    update 1

--- Web_Programming_with_Python_and_JavaScript/05_SQL_Models_Migrations/airline/flights/models.py
+++ Web_Programming_with_Python_and_JavaScript/05_SQL_Models_Migrations/airline/flights/models.py
@@ -6,8 +6,31 @@ from django.db import models
 # one model for each of the created tables for information storage
 # each model will be a separate class
 #
+
+# After change in models.py ->
+# py manage.py makemigrations 
+# py manage.py migrate
+# back into the shell -> py manage.py shell
+# !! pay attention command is executed in the folder where manage.py !!
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    # airport code for example MUC for Munich
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    # if I delete an airport, models.CASCADE will also delete any corresponding flights
+    # that are part of other table
+    # related name gives info in oposite direction
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
     
+    # in every class we can define a funcion __str__ that implemets a string representation of the object
+    # instead of calling it Object 1 etc.
+
+    def __str__(self) -> str:
+        return f"{self.id}: {self.origin} to {self.destination}"
