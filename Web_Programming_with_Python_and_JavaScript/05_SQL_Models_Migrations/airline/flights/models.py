f61c13993d26b1c3d7f3383cc6b8304d23299048
Dr. Delyan Malchev <39366200+DrMalchev@users.noreply.github.com>
2021-09-03 11:54:34 +0300

    Booking passengers working state

--- Web_Programming_with_Python_and_JavaScript/05_SQL_Models_Migrations/airline/flights/models.py
+++ Web_Programming_with_Python_and_JavaScript/05_SQL_Models_Migrations/airline/flights/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields.related import ManyToManyField
 
 # Create your models here.
 
@@ -34,3 +35,13 @@ class Flight(models.Model):
 
     def __str__(self) -> str:
         return f"{self.id}: {self.origin} to {self.destination}"
+
+class Passenger(models.Model):
+    
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+    # related name gives access of Flight to Passengers
+
+    def __str__(self):
+        return f"{self.first} {self.last}"
--- Web_Programming_with_Python_and_JavaScript/05_SQL_Models_Migrations/airline/flights/views.py
+++ Web_Programming_with_Python_and_JavaScript/05_SQL_Models_Migrations/airline/flights/views.py
@@ -1,8 +1,11 @@
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 
-from .models import Flight, Airport
+from .models import Flight, Passenger
+from django.urls import reverse
 
 def index(request):
     return render(request, "flights/index.html", {
@@ -10,10 +13,24 @@ def index(request):
     })
 
 def flight(request, flight_id):
+    
     flight = Flight.objects.get(id=flight_id)
     # alternatively I can use pk (primary key) which is id
     #flight = Flight.objects.get(pk=flight_id)
     return render(request, "flights/flight.html", {
-        "flight": flight
+        "flight": flight,
+        "passengers": flight.passengers.all(), 
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
+        # passengers is the related name in models.py
 
     })
+
+def book(request, flight_id):
+    passenger_id = int(request.POST["passenger"])
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=passenger_id) 
+        # info will be parsed by a form with input field named passenger
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
+        # reverse takes the name of particular view and finds the url from url paths
