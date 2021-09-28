06c3506992c78f27a99420c4415d09c561c1d6d2
Dr. Delyan Malchev <39366200+DrMalchev@users.noreply.github.com>
2021-09-28 11:34:49 +0300

    Archive function added and some further Harvard CS50W requirements cleared

--- Web_Programming_with_Python_and_JavaScript/Project_3_Mail/mail/mail/static/mail/inbox.js
+++ Web_Programming_with_Python_and_JavaScript/Project_3_Mail/mail/mail/static/mail/inbox.js
@@ -1,18 +1,18 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-  
+
   // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox')); 
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click',  compose_email);
-  
+  document.querySelector('#compose').addEventListener('click', compose_email);
+
   // By default, load the inbox
-  
-  if(name == 'inbox')  {load_mailbox('inbox');}
-  else {load_mailbox(name);}
-  
-  
+
+  if (name == 'inbox') { load_mailbox('inbox'); }
+  else { load_mailbox(name); }
+
+
 });
 
 
@@ -32,9 +32,9 @@ function compose_email() {
   document.getElementsByTagName("INPUT")[3].addEventListener('click', send_email);
 
   const elements = document.getElementsByClassName('mailboxDiv');
-    while(elements.length > 0){
-        elements[0].parentNode.removeChild(elements[0]);
-    }
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
 
   return false;
 }
@@ -45,43 +45,43 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#show_email').style.display = 'none';
-  
-  
+
+
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-  name=mailbox;
+  name = mailbox;
 
   const elements = document.getElementsByClassName('mailboxDiv');
-    while(elements.length > 0){
-        elements[0].parentNode.removeChild(elements[0]);
-    }
-  
-  
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+
+
   //get data from mailbox
   fetch(`/emails/${mailbox}`)
     .then(response => response.text())
-    .then(text => { 
+    .then(text => {
       console.log(JSON.parse(text));
       JSON.parse(text).forEach(element => {
-        
+
         console.log(element);
 
         let myDiv = document.createElement('div');
         myDiv.className = "mailboxDiv";
         myDiv.id = `id${element.id}`;
         //myDiv.onmouseover = function() {};
-        
-        myDiv.innerHTML=`<span style="margin-right:40px; font-weight:bold;">${element.sender} </span> <span>${element.subject}</span>  <span style="float:right;">${element.timestamp}</span>` ;
-        
+
+        myDiv.innerHTML = `<span style="margin-right:40px; font-weight:bold;">${element.sender} </span> <span>${element.subject}</span>  <span style="float:right;">${element.timestamp}</span>`;
+
         document.body.appendChild(myDiv);
-        document.querySelector(`#id${element.id}`).addEventListener('click', () => load_email(element.id));
+        document.querySelector(`#id${element.id}`).addEventListener('click', () => load_email(element.id, mailbox));
         console.log(element.id);
-        
-        if (element.read===true){myDiv.style.cssText ='border:1px solid black; max-width: 1024px; ; padding: 10px 15px 10px 15px; margin-left: 5%; margin-right:10%;background: lightgrey';}
-        else {myDiv.style.cssText ='border:1px solid black; max-width: 1024px; ; padding: 10px 15px 10px 15px; margin-left: 5%; margin-right:10%';}
+
+        if (element.read === true) { myDiv.style.cssText = 'border:1px solid black; max-width: 1024px; ; padding: 10px 15px 10px 15px; margin-left: 5%; margin-right:10%;background: lightgrey'; }
+        else { myDiv.style.cssText = 'border:1px solid black; max-width: 1024px; ; padding: 10px 15px 10px 15px; margin-left: 5%; margin-right:10%'; }
       });
     })
-    .catch(err => console.error(err));  
+    .catch(err => console.error(err));
 
 
 
@@ -96,43 +96,44 @@ function send_email() {
       subject: document.querySelector('#compose-subject').value,
       body: document.querySelector('#compose-body').value
     }),
-    headers: {'Content-type': 'application/json; charset=UTF-8'}
+    headers: { 'Content-type': 'application/json; charset=UTF-8' }
   })
     .then(response => response.json())
     .then(result => {
       console.log("Email status ckeck: ", result);
-      
+
       //alert("Email sent successfully!");
       //window.stop(); 
-      
-      
+
+
     })
     .catch(err => console.error(err));
-      let name = 'sent';
-      load_mailbox('sent');
-      //alert("Email sent successfully!");
-      
-  
+  let name = 'sent';
+  load_mailbox('sent');
+  //alert("Email sent successfully!");
+
+
 }
 
 
-function load_email(element_id){
+function load_email(element_id, mailbox) {
   // email is clicked so mark it as read (grey background)
   document.getElementById('show_email').innerHTML = "";
-  fetch (`/emails/${element_id}`, {
+  fetch(`/emails/${element_id}`, {
     method: 'PUT',
     body: JSON.stringify({ read: true }),
-    headers: {'Content-type': 'application/json; charset=UTF-8'}
+    headers: { 'Content-type': 'application/json; charset=UTF-8' }
 
   })
 
   console.log(`Yuhuuuuuu ${element_id}`)
+  console.log("Mailbox: "+mailbox +"  yuhuuuuu")
   // Hide compose view emails view
   // show show email view
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#show_email').style.display = 'block';
-  document.querySelectorAll('.mailboxDiv').forEach(div => {div.style.display = 'none';})
+  document.querySelectorAll('.mailboxDiv').forEach(div => { div.style.display = 'none'; })
   //test how getElementById works
   //document.getElementById('show_email').appendChild(document.createElement('hr'))
 
@@ -142,52 +143,96 @@ function load_email(element_id){
       //console.log(text.sender);
       let myDivSender = document.createElement('div');
       myDivSender.className = "emailDiv";
-      myDivSender.innerHTML=`From: ${text.sender}`;
+      myDivSender.innerHTML = `From: ${text.sender}`;
       document.getElementById('show_email').appendChild(myDivSender);
 
       let myDivRecipient = document.createElement('div');
       myDivRecipient.className = "emailDiv";
-      myDivRecipient.innerHTML=`To: ${text.recipients}`;
+      myDivRecipient.innerHTML = `To: ${text.recipients}`;
       document.getElementById('show_email').appendChild(myDivRecipient);
 
       let myDivSubject = document.createElement('div');
       myDivSubject.className = "emailDiv";
-      myDivSubject.innerHTML=`Subject: ${text.subject}`;
+      myDivSubject.innerHTML = `Subject: ${text.subject}`;
       document.getElementById('show_email').appendChild(myDivSubject);
 
       let myDivTime = document.createElement('div');
       myDivTime.className = "emailDiv";
-      myDivTime.innerHTML=`Timestamp: ${text.timestamp}`;
+      myDivTime.innerHTML = `Timestamp: ${text.timestamp}`;
       document.getElementById('show_email').appendChild(myDivTime);
 
+      if (mailbox!='sent') {
       let myDivButton = document.createElement('button');
       myDivButton.className = "replyButton";
-      myDivButton.innerHTML='Reply';
+      myDivButton.id = "rButton";
+      myDivButton.innerHTML = 'Reply';
       document.getElementById('show_email').appendChild(myDivButton);
 
+      let myDivButton2 = document.createElement('button');
+      myDivButton2.className = "archiveButton";
+      myDivButton2.id = "aButton";
+      myDivButton2.innerHTML = 'Archive';
+      document.getElementById('show_email').appendChild(myDivButton2);
+      }
       let myHr = document.createElement('hr');
       myHr.className = "myHr";
       document.getElementById('show_email').appendChild(myHr);
 
       let myDivBody = document.createElement('div');
       myDivBody.className = "emailBodyDiv";
-      myDivBody.innerHTML=`Timestamp: ${text.body}`;
+      myDivBody.innerHTML = `Timestamp: ${text.body}`;
       document.getElementById('show_email').appendChild(myDivBody);
-  });  
-    
-        
-        
 
-        
-        
-         
-        
-        
-        
-        
-        
-        
+      if (text.archived===true) {document.querySelector('#aButton').innerHTML="Unarchive";}
+      else {document.querySelector('#aButton').innerHTML="Archive";}
+      document.querySelector('#aButton').addEventListener('click', () => archive_email(element_id));
+      document.querySelector('#rButton').addEventListener('click', () => reply_email(element_id));
+    });  //fetch close brackets
+
+
+
+}
+
+function archive_email(element_id) {
+  
+  
+  fetch(`/emails/${element_id}`)
+  .then(response => response.json())
+  .then(result => {
+      console.log("Archived: ", result.archived);
+      if (result.archived===false) {
+        console.log("Archive id: " + element_id);
+        fetch(`/emails/${element_id}`, {
+          method: 'PUT',
+          body: JSON.stringify({ archived: true }),
+          headers: { 'Content-type': 'application/json; charset=UTF-8' }
       
-   // })
+        })
+        document.querySelector('#aButton').innerHTML="Unarchive";
+        }//eundif
+      else {
+        
+        console.log("Archive id: " + element_id);
+        fetch(`/emails/${element_id}`, {
+          method: 'PUT',
+          body: JSON.stringify({ archived: false }),
+          headers: { 'Content-type': 'application/json; charset=UTF-8' }
+      
+        })
+        document.querySelector('#aButton').innerHTML="Archive";
+      }
+      //alert("Email sent successfully!");
+      //window.stop(); 
+
+
+    })
+  
+
+}
+
+function reply_email(element_id) {
+  
+  console.log("Reply id: " + element_id);
+
 
 }
